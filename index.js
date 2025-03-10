ccfaf800be4f6ca5f1328d88fa9b7e2daae0b374
elvis <kariuki.elvis@student.moringaschool.com>
2025-03-10 12:10:48 +0300

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    return names.map(name => {
+      return `Thank you, ${name}, for the wonderful ${event} gift!`;
+    });
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
